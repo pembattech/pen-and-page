abf7dc6354c37129642e72052b74c5233e3e4075
pembattech <pembat.tech@gmail.com>
2023-07-10 10:16:10 +0545

    minor changes

--- home/urls.py
+++ home/urls.py
@@ -3,12 +3,12 @@ from .views import *
 
 urlpatterns = [
     path("", home, name  = "home"),
-    path("addblog/", add_blog, name = "addblog"),
+    path("new/", add_blog, name = "addblog"),
     path("login/", login_view, name = "login" ),
     path("register/", register_view, name = "register" ),
     path("logout/", logout_view, name = "logout" ),
-    path("blog_detail/<slug>", blog_detail, name = "blog_detail" ),
-    path("blog_category/<category>", blog_category, name = "blog_category" ),
+    path("blog/<slug>", blog_detail, name = "blog_detail" ),
+    path("category/<category>", blog_category, name = "category" ),
     path("profile/<int:authorid>", author_profile, name = "profile"),
     path("delete_blog/<id>", delete_blog, name = "delete_blog"),
     path("edit_blog/<slug>", edit_blog, name = "edit_blog"),
